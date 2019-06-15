26296b59dac5f7cedfd02a870cb6728b893f0472
Edson Onildo <inbox.edsononildo@gmail.com>
2019-06-15 11:22:07 -0300

    habilitando extencao .php para arquivos de template

--- src/Content.php
+++ src/Content.php
@@ -7,7 +7,7 @@ class Content
     public static function getContent(string $view): string
     {
         $view = str_replace('.', '/', $view);
-        $file = Tpl::getDir() . "$view.html";
+        $file = Tpl::getDir() . "$view.php";
 
         if (!file_exists($file)) {
             throw new \Exception("$file template not found"); // @codeCoverageIgnore
