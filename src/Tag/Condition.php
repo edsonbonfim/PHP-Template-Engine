c8950a293f755a4226eab4f8aed4f5d87378ca50
Edson Onildo <inbox.edsononildo@gmail.com>
2019-12-01 20:27:07 -0300

    Corrigindo problema de multiplos parenteses

--- src/Tag/Condition.php
+++ src/Tag/Condition.php
@@ -14,13 +14,13 @@ class Condition extends Tag
 
     public function if()
     {
-        self::match('/@\s*if\s*\((.*?)\)/', function($cond) {
+        self::match('/\s*@\s*if\s*\((.*?)\)\s*$/m', function($cond) {
             self::replace("<?php if ($cond) : ?>");
         });
     }
 
     public function elseif() {
-        self::match('/@\s*elseif\s*\((.*?)\)/', function($cond) {
+        self::match('/\s*@\s*elseif\s*\((.*?)\)\s*$/m', function($cond) {
             self::replace("<?php elseif ($cond) : ?>");
         });
     }
